c6878b79455beb64fe697437941c92fe1afa1134
BeanBeing <doomknightdoom@gmail.com>
2023-08-16 12:52:10 -0400

    Fixed memory issues and segmentation faults

--- main.c
+++ main.c
@@ -4,33 +4,34 @@
 
 int main(int argc, char *argv[])
 {
-  char* str = malloc(strlen(argv[1]) + 1);
+   char str[1024] = {0};
+
+   // argument limit...
+   if(argc > 128){
+      puts("TOO MANY ARGUMENTS! MAKE SURE TO ONLY PASS <argc> OF ARGUMENTS!");
+      return 1;
+   }
+
+   // Concatenates every all argv towards str
+   for(int i = 1; i < argc; i++){
+      strncat(str, argv[i], 1023);
+      strcat(str, " ");
+   }
+
+   // Gets length of str
+   int length = strlen(str);
+
+   // For loop to reverse str
+   for(int i = 0; i < length / 2; i++){
+      char temp = str[i];
+      str[i] = str[length - 1 - i];
+      str[length - 1 - i] = temp;
+   }
  
-  // argument limit...
-  if(argc > 2){
-    puts("TOO MANY ARGUMENTS! MAKE SURE TO ONLY PASS <argc> OF ARGUMENTS!");
-    return 1;
-  }
-
-  // catonates all arguments along with space for each argument to str
-    strcpy(str, argv[1]);
-    strcat(str, " ");
-
-  // Gets length of str
-  int length = strlen(str);
-
-  // For loop to reverse str
-  for(int i = 0; i < length / 2; i++){
-    char temp = str[i];
-    str[i] = str[length - 1 - i];
-    str[length - 1 - i] = temp;
-  }
- 
-  // Print reversed string 
-  fputs(str, stdout);
-  putchar(' ');	// Places empty space afterwards
-  putchar('\n');	// New line
-  free(str);	// Frees str malloc
-  return 0;
+   // Print reversed string 
+   fputs(str, stdout);
+   putchar(' ');	// Places empty space afterwards
+   putchar('\n');	// New line
+   return 0;
 }
     
